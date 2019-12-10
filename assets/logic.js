23524735a2ab4ebaef59d26b6b04cb79ec356486
Nommington <rymckenz@gmail.com>
2019-12-10 00:46:39 -0500

    Started naming variables

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,12 @@
-var name = ["", "", "binary", "tertiary", "quaternary", "quinary", "senary", "septenary", "octal", "nonary", "decimal"];
+/* Define variables including:
+user-provided
+-number
+-base
+*/
+
+
+var num = 0;
+var base = 0;
+var nameArr = ["decimal", "", "binary", "tertiary", "quaternary", "quinary", "senary", "septenary", "octal", "nonary", "other"];
+var name = nameArr[base];
+
